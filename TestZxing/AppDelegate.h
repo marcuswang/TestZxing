ef3626ddfd5eefddfdc6fb8675ffaf74ca8f55cc
老邢 Thierry <thierry.xing@gmail.com>
2013-02-25 19:11:20 +0800

    Initial Commit

---
+++ TestZxing/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestZxing
+//
+//  Created by 老邢 Thierry on 13-2-25.
+//  Copyright (c) 2013年 Enway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestZxing/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestZxing
+//
+//  Created by 老邢 Thierry on 13-2-25.
+//  Copyright (c) 2013年 Enway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
