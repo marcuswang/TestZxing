5f3f3e42b7eb7da61ec652f7cd34b35f8f1ed710
wangjun <wangjun@witmob.com>
2013-07-31 13:01:52 +0800

    first commit

---
+++ TestZxing/CameraController.h
@@ -0,0 +1,13 @@
+//
+//  CameraController.h
+//  TestZxing
+//
+//  Created by 老邢 Thierry on 13-2-26.
+//  Copyright (c) 2013年 Enway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CameraController : UIViewController
+
+@end
---
+++ TestZxing/CameraController.mm
@@ -0,0 +1,38 @@
+//
+//  CameraController.m
+//  TestZxing
+//
+//  Created by 老邢 Thierry on 13-2-26.
+//  Copyright (c) 2013年 Enway. All rights reserved.
+//
+
+#import "CameraController.h"
+
+@interface CameraController ()
+
+@end
+
+@implementation CameraController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
